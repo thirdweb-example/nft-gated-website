a82f76da0b7a3c6db6e7f662e6045ad1892f599d
Jarrod Watts <jarrodwatts16@gmail.com>
2022-07-27 12:35:13 +1000

    include logout and authenticate routes

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,9 +1,14 @@
 import { useAddress, useMetamask, useSDK } from "@thirdweb-dev/react";
 import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
   const router = useRouter();
+
+  const [loadingAuthStatus, setLoadingAuthStatus] = useState(true);
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
   // Wallet & Network Information
   const address = useAddress();
   const connectWithMetamask = useMetamask();
@@ -15,7 +20,6 @@ export default function Home() {
   async function signIn() {
     // Add the domain of the application users will login to, this will be used throughout the login process
     const domain = "thirdweb.com";
-
     // Generate a signed login payload for the connected wallet to authenticate with
     const payload = await sdk.auth.login(domain);
 
@@ -29,13 +33,35 @@ export default function Home() {
     });
 
     if (response.ok) {
-      router.push("/restricted-page");
+      setIsAuthenticated(true);
     } else {
       const data = await response.json();
       console.log(data);
       alert(data.error);
     }
   }
+
+  async function signOut() {
+    await fetch("/api/logout", {
+      method: "POST",
+    });
+
+    setIsAuthenticated(false);
+  }
+
+  // When the page loads, check if the user is already authenticated via the cookie using the /authenticate route.
+  useEffect(() => {
+    (async () => {
+      const res = await fetch("/api/authenticate", {
+        method: "POST",
+      });
+
+      if (res.ok) {
+        setIsAuthenticated(true);
+      }
+      setLoadingAuthStatus(false);
+    })();
+  }, []);
 
   return (
     <div className={styles.container}>
@@ -57,18 +83,42 @@ export default function Home() {
         !
       </p>
 
-      {address ? (
-        <button
-          className={styles.mainButton}
-          style={{ width: "fit-content", paddingRight: 16, paddingLeft: 16 }}
-          onClick={signIn}
-        >
-          Sign In With Ethereum
-        </button>
+      <hr className={styles.divider} />
+
+      {loadingAuthStatus ? (
+        <p>Checking authentication status...</p>
+      ) : isAuthenticated && address ? (
+        <>
+          <b>You are connected and authenticated!</b>
+
+          <button
+            className={styles.secondaryButton}
+            style={{ width: 256, margin: 24 }}
+            onClick={signOut}
+          >
+            Logout
+          </button>
+
+          <p>Welcome, {address.slice(0, 6)}...</p>
+
+          <button
+            className={styles.mainButton}
+            style={{ width: 256 }}
+            onClick={() => router.push(`/restricted-page`)}
+          >
+            View Restricted Page
+          </button>
+        </>
       ) : (
-        <button onClick={connectWithMetamask} className={styles.mainButton}>
-          Connect Wallet
-        </button>
+        <>
+          <button
+            className={styles.mainButton}
+            style={{ width: "fit-content", paddingRight: 16, paddingLeft: 16 }}
+            onClick={() => (address ? signIn() : connectWithMetamask())}
+          >
+            {address ? "Sign In With Ethereum" : "Connect Wallet"}
+          </button>
+        </>
       )}
     </div>
   );
