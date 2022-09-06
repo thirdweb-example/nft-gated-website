8070818f511428311481e67c5f9754de5866cb68
Jarrod Watts <35651410+jarrodwatts@users.noreply.github.com>
2022-09-06 11:56:08 +1000

    Update ThirdwebGuideFooter.jsx

--- components/ThirdwebGuideFooter.jsx
+++ components/ThirdwebGuideFooter.jsx
@@ -18,7 +18,7 @@ export default function ThirdwebGuideFooter() {
         role="button"
         onClick={() =>
           window.open(
-            "https://github.com/thirdweb-example/authentication-restricted-content",
+            "https://github.com/thirdweb-example/nft-gated-website",
             "_blank"
           )
         }
@@ -39,7 +39,7 @@ export default function ThirdwebGuideFooter() {
           style={{ cursor: "pointer" }}
           onClick={() =>
             window.open(
-              "https://github.com/thirdweb-example/authentication-restricted-content",
+              "https://github.com/thirdweb-example/nft-gated-website",
               "_blank"
             )
           }
