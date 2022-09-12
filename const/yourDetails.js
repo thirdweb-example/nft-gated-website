36d553ecbeaea56d1484c8145aa1175a1818a848
Jarrod Watts <35651410+jarrodwatts@users.noreply.github.com>
2022-09-12 15:06:54 +1000

    change to example.com

--- const/yourDetails.js
+++ const/yourDetails.js
@@ -1,5 +1,5 @@
 // Replace this with your contract address
 export const contractAddress = "0x1fCbA150F05Bbe1C9D21d3ab08E35D682a4c41bF";
 
-// Replace this with your domain name (or any other name you want)
-export const domainName = "example.org";
+// Replace this with your domain name.
+export const domainName = "example.com";
