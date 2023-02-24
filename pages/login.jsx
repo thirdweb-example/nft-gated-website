1e50ae2f9609ab4fce4459f0d89da73c0ec438f0
Utkarsh <54569941+utkarshxy@users.noreply.github.com>
2023-02-24 14:45:36 +0530

    login.jsx where quotes were  fixed with &quot;

--- pages/login.jsx
+++ pages/login.jsx
@@ -3,63 +3,78 @@ import Link from "next/link";
 import styles from "../styles/Home.module.css";
 
 // replace this with your contract address
-const contractAddress = "0x1fCbA150F05Bbe1C9D21d3ab08E35D682a4c41bF";
+const contractAddress = "0x9c1361fFA29445297711ce5cE5275EFF3D2a79c0";
 
 export default function Login() {
   const address = useAddress(); // Get the user's address
 
   return (
     <div className={styles.container}>
-      <h1 className={styles.h1}>Auth - NFT Gated Content</h1>
-      <p className={styles.explain}>
-        Serve exclusive content to users who own an NFT from your collection,
-        using{" "}
-        <b>
-          <a
-            href="https://portal.thirdweb.com/building-web3-apps/authenticating-users"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.purple}
-          >
-            Auth
-          </a>
-        </b>
-        !
-      </p>
+      <h1 className={styles.h1}>
+        Exclusive and Early Access to Some Exciting Information
+      </h1>
 
       <p className={styles.explain}>
-        You cannot access the{" "}
-        <Link className={styles.purple} href="/">
-          main page
-        </Link>{" "}
-        unless you own an NFT from our collection!
+        brought to you by{" "}
+        <b>
+          <a
+            href="https://www.mantle.xyz/developers"
+            target="_blank"
+            rel="noopener noreferrer"
+            className={styles.mantleGreen}
+          >
+            Team Mantle
+          </a>
+        </b>
       </p>
 
       <hr className={styles.divider} />
 
+      <p></p>
+      <p></p>
+
+      <h1 className={styles.h2}>Step 1</h1>
+      <p>Connect your wallet with Mantle Testnet.</p>
       <>
         {address ? (
           <p>
-            Welcome, {address?.slice(0, 6)}...{address?.slice(-4)}
+            Connected to {address?.slice(0, 6)}...{address?.slice(-4)}
           </p>
         ) : (
-          <p>Please connect your wallet to continue.</p>
+          <p></p>
         )}
 
-        <ConnectWallet accentColor="#F213A4" />
-
-        <p>
-          For demo purposes, you can claim an NFT from our collection below:
-        </p>
-
-        <Web3Button
-          contractAddress={contractAddress}
-          action={(contract) => contract.erc1155.claim(0, 1)}
-          accentColor="#F213A4"
-        >
-          Claim NFT
-        </Web3Button>
+        <ConnectWallet accentColor="#65b3ae"/>
       </>
+
+      <p></p>
+      <p></p>
+
+      <h1 className={styles.h2}>Step 2</h1>
+      <p>Please click on the &quot;Claim NFT&quot; that gives you the access.</p>
+
+      <Web3Button
+        contractAddress={contractAddress}
+        action={(contract) => contract.erc1155.claim(0, 1)}
+        accentColor="#65b3ae"
+      >
+        Claim NFT
+      </Web3Button>
+
+      <p></p>
+      <p></p>
+
+      <h1 className={styles.h2}>Step 3</h1>
+      <p>Once the transaction is confirmed, click on &quot;Are you ready&quot; below.</p>
+
+      <p className={styles.explain}>
+        {" "}
+        <Link className={styles.mantleGreen} href="/">
+          Are You Ready?
+        </Link>{" "}
+      </p>
+
+      <hr className={styles.divider} />
     </div>
   );
 }
